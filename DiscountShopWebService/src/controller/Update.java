3d82a2c17087918bb88e439b66dcece61bf5a242
Jingyu Huang <jingyuh@andrew.cmu.edu>
2015-04-11 13:25:46 -0700

    final

--- DiscountShopWebService/src/controller/Update.java
+++ DiscountShopWebService/src/controller/Update.java
@@ -6,6 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 import model.Item;
 import model.Retailer;
 //already act as a web service
@@ -14,8 +18,9 @@ public class Update {
 	private Statement statement = null;
 	private String sql = null;
 	
-	public void updateAddress(){
-		
+	public boolean updateAddress(String username, String newAddress){
+		String sql = "UPDATE retailers SET address ='" + newAddress + "' WHERE username ='" + username + "';";
+		return databaseUtil(sql);
 	}
 	
 	
@@ -44,5 +49,57 @@ public class Update {
 		connection.close();
 	    
 	}
+	
+	private boolean databaseUtil(String sql) {
+		try {
+			Context ctx = (Context) new InitialContext()
+					.lookup("java:comp/env");
+			connection = ((DataSource) ctx.lookup("jdbc/mysql"))
+					.getConnection();
+
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+//			ResultSet keys=statement.getGeneratedKeys();
+//			int id=-1;
+//			while(keys.next()){
+//				id=keys.getInt(1);
+//			}
+			statement.close();
+			statement = null;
+
+			connection.close();
+			connection = null;
+			
+//			keys.close();
+//			
+//			if(id==-1)
+//			return false;	
+//			else
+			return true;
+			
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqlex) {
+					// ignore -- as we can't do anything about it here
+				}
+				statement = null;
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqlex) {
+					// ignore -- as we can't do anything about it here
+				}
+				connection = null;
+			}
+		}
+		return false;
+	}
+
 	
 }
--- DiscountShopWebService/src/view/UpdateServlet.java
+++ DiscountShopWebService/src/view/UpdateServlet.java
@@ -1,11 +1,21 @@
 package view;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Consumer;
+import model.Retailer;
+
+import com.google.gson.JsonObject;
+
+import controller.CreateUser;
+import controller.Update;
 
 //for future implementation
 @WebServlet("/UpdateServlet")
@@ -24,14 +34,51 @@ public class UpdateServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doPost(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		response.setHeader("Cache-control", "no-cache, no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "-1");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		response.setHeader("Access-Control-Max-Age", "86400");
+
+		//update type comming from Android
+		String updateType=request.getParameter("updateType").trim();
+		//get user info and populate user
+		boolean successStatus=false;
+		Update update=new Update();
+		if(updateType.equals("address")){
+			String username=request.getParameter("username").trim();
+			String newAddress=request.getParameter("newAddress").trim();
+
+			successStatus=update.updateAddress(username, newAddress);
+		}
+		else{
+			//for future implementation
+		}
+		
+		// if success
+		if(successStatus){
+			JsonObject myObj=new JsonObject();
+			myObj.addProperty("success", true);
+			out.println(myObj.toString());
+		}
+		// if failure
+		else{
+			JsonObject myObj=new JsonObject();
+			myObj.addProperty("success", false);
+			out.println(myObj.toString());
+		}
+		out.close();
 	}
 
 }
