bedbfb229dcb47a2689996658b5b3b182d848d9b
Jingyu Huang <jingyuh@andrew.cmu.edu>
2015-04-10 17:11:23 -0700

    first

---
+++ DiscountShopWebService/src/controller/LoginInfo.java
@@ -0,0 +1,76 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import model.Login;
+
+public class LoginInfo {
+
+	private Connection connection = null;
+	private PreparedStatement statement = null;
+	private String sql = null;
+
+	public Login getInfo(String loginType, String username) {
+		Login login = new Login();
+
+		try {
+			Context ctx = (Context) new InitialContext()
+					.lookup("java:comp/env");
+			connection = ((DataSource) ctx.lookup("jdbc/mysql"))
+					.getConnection();
+			// consumer
+			if (loginType.equals("consumer")) {
+				sql = "Select * from consumers where username = ?";
+			}
+			//
+			else {
+				sql = "Select * from retailers where username = ?";
+			}
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, username);
+			ResultSet rs = statement.executeQuery();
+
+			while (rs.next()) {
+				login.setUsername(rs.getString("username").trim());
+				login.setPassword(rs.getString("password").trim());
+			}
+
+			rs.close();
+			statement.close();
+			statement = null;
+
+			connection.close();
+			connection = null;
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqlex) {
+					// ignore -- as we can't do anything about it here
+				}
+				statement = null;
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqlex) {
+					// ignore -- as we can't do anything about it here
+				}
+				connection = null;
+			}
+		}
+
+		return login;
+	}
+
+}
---
+++ DiscountShopWebService/src/view/LoginServlet.java
@@ -0,0 +1,77 @@
+package view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Login;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import controller.LoginInfo;
+
+public class LoginServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -7003289728882047706L;
+
+	public LoginServlet(){
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		response.setHeader("Cache-control", "no-cache, no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "-1");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+		response.setHeader("Access-Control-Max-Age", "86400");
+
+		//username coming from Android
+		String username= request.getParameter("username").trim();
+		//login type comming from Android
+		String loginType=request.getParameter("loginType").trim();
+		//get login info based on username and loginType
+		LoginInfo loginInfo = new LoginInfo();
+		Login login = loginInfo.getInfo(loginType,username);
+		
+		// if invalid username
+		if(login.getUsername()==null){
+			JsonObject myObj=new JsonObject();
+			myObj.addProperty("success", false);
+			out.println(myObj.toString());
+		}
+		// if a valid username was sent
+		else{
+			Gson gson = new Gson();
+			//create json from login object
+			JsonElement loginObj=gson.toJsonTree(login);
+			//create a new JSON object
+			JsonObject myObj=new JsonObject();
+			//add property as success;
+			myObj.addProperty("success", true);
+			//add login object
+			myObj.add("loginInfo", loginObj);
+			//convert the JSON to string and send back
+			out.println(myObj.toString());
+		}
+		out.close();
+		
+
+	}
+}
